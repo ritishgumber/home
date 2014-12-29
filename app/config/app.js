3db5283a22834a14e3d40966550123202a7cfb6e
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2014-12-29 10:48:50 +0530

    Changed Server URL

--- app/config/app.js
+++ app/config/app.js
@@ -1,3 +1,3 @@
 var app=angular.module('cbLanding',['ui.router','picardy.fontawesome']);
-//var serverURL="http://localhost:3000";
-var serverURL="http://cloudbooostfrontendservices.azurewebsites.net";
+var serverURL="http://localhost:3000";
+//var serverURL="http://cloudbooostfrontendservices.azurewebsites.net";
