411806911455abd7b2b3bcd27807929ceb4df176
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-02 19:50:34 +0530

    Changed BAckend URL's

--- app/config/app.js
+++ app/config/app.js
@@ -1,4 +1,4 @@
 var app=angular.module('cbLanding',['ui.router','picardy.fontawesome']);
 //var serverURL="http://localhost:3000";
-var serverURL="http://cbfrontendservice.azurewebsites.net";
+var serverURL="http://service.cloudboost.io";
 var dashboardURL = "https://dashboard.cloudboost.io";
