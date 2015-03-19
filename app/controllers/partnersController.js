48f4bc889296ac0bcdee3efbb621c4196309f588
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-19 13:17:35 +0530

    PArtner page added

---
+++ app/controllers/partnersController.js
@@ -0,0 +1,19 @@
+app.controller('partnersController',['$scope',function($scope){
+    $scope.getCssClass ="features-page";
+
+    $scope.init = function(){
+
+        var meta=document.getElementsByTagName("meta");
+        for (var i=0; i<meta.length; i++) {
+            if (meta[i].name.toLowerCase()=="description") {
+                meta[i].content= "CloudBoost Partners : Join the CloudBoost Partner Program to earn client revenue share and access exclusive resources.";
+            }
+        }
+
+        document.title = "CloudBoost Partners.";
+        
+        
+    };
+
+
+ }]);
