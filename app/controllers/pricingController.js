f1bc14b3e614060a9acc111a20c7fe4396cbb83d
RTBathula <battu.network@gmail.com>
2015-03-22 20:13:45 +0530

    fixed bugs

--- app/controllers/pricingController.js
+++ app/controllers/pricingController.js
@@ -1,5 +1,6 @@
-app.controller('pricingController',['$scope',function($scope){
+app.controller('pricingController',['$scope','$rootScope',function($scope,$rootScope){
   $scope.getCssClass ="pricing-page";
+  $rootScope.showHeader=true;
 
   $scope.faqExpand = function(id){
   	if($('#'+id).hasClass( "in" )){
@@ -9,8 +10,8 @@ app.controller('pricingController',['$scope',function($scope){
   	}
   };
 
-  $scope.init = function(){
-  		var meta=document.getElementsByTagName("meta");
+  $scope.init = function(){    
+  	var meta=document.getElementsByTagName("meta");
 		for (var i=0; i<meta.length; i++) {
 		    if (meta[i].name.toLowerCase()=="description") {
 		        meta[i].content= "CloudBoost Pricing. Free plan to begin with and pay as you go as you scale.  ";
