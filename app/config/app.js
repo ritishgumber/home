5987c5f78ec6d03e74be0e2390b02c9b774182e0
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-23 08:54:36 +0530

    isDevelopment automated

--- app/config/app.js
+++ app/config/app.js
@@ -1,5 +1,9 @@
 var __isDevelopment = false;
 
+if(window.location.host.indexOf('localhost') > -1){
+	__isDevelopment = true;
+}
+
 var app=angular.module('cbLanding',['ui.router','picardy.fontawesome', 'seo','ngCookies']);
 
 var serverURL = null, dashboardURL = null;
