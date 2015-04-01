d8ceddad99f787d11374c2396e94c205966e2014
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-04-01 18:37:12 +0530

    Uninstalled PhantomJs

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.use(require('prerender-node').set('prerenderToken', 'nYvFcfW9ZFKOxaTP8Vsj'))
 app.set('port', process.env.PORT || 1444);
 
 var server = app.listen(app.get('port'), function() {
+	
 });
