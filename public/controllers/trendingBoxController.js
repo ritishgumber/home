9896e8959e1be7df51a8af53009ddf4eba446a9d
rtbathula <battu.network@gmail.com>
2015-12-28 20:06:06 -0300

    update

--- public/controllers/trendingBoxController.js
+++ public/controllers/trendingBoxController.js
@@ -33,14 +33,44 @@ var onreadytodemo=function(e){
 
 var onreadyToSearch=function(e){
     $(".animsec1 .mobile-screen-inn").addClass("goToMblSlide2"); 
-    $(".animsec1 .goToMblSlide2 .mbl-slide2 .mbl-search-default")[0].addEventListener( 'webkitTransitionEnd', onreadytoshowspinner, false ); 
+    $(".animsec1 .goToMblSlide2 .mbl-slide2 .mbl-app-searchin-wrap .search-icon .searchicon-mode2")[0].addEventListener( 'webkitTransitionEnd', showspinner, false ); 
 }
 
-var onreadytoshowspinner=function(e){
-    $(".animsec1 .mbl-slide2 .mbl-app-searchin-wrap").addClass("showspinner");
-    $(".animsec1 .goToMblSlide2 .mbl-slide2 .showspinner .search-icon")[0].addEventListener( 'webkitTransitionEnd', onreadyToShowResults, false ); 
+var showspinner=function (e) {
+    $(".animsec1 .mobile-screen-inn").addClass("showspinner"); 
+    $(".animsec1 .showspinner .mbl-slide2 .mbl-app-searchin-wrap .search-icon .searchspinner")[0].addEventListener( 'webkitTransitionEnd', onreadyToShowResults, false ); 
 }
-
 var onreadyToShowResults=function (e) {
     $(".mobile-screen-inn").addClass("showsearchresults");
+    $(".animsec1 .showsearchresults .mbl-slide2 .mbl-app-content")[0].addEventListener( 'animationend', ondemofinished, false );  
 }
+
+var ondemofinished=function (e) {
+    $(".trending-slides").addClass("slidedemofinished");
+    $(".slidedemofinished .animsec2 .graphic-column3 .demo-explain2")[0].addEventListener( 'animationend', onreadyforrelatime, false );
+}
+
+var onreadyforrelatime=function (e) {
+    $(".trending-slides").addClass("readyforrealtime");
+    $(".readyforrealtime .animsec1 .mbl-slide1 .mbl-app-notify-bubble")[0].addEventListener( 'webkitTransitionEnd', getslide2, false ); 
+}
+
+var getslide2=function (e) {
+    $(".trending-slides").addClass("goToSlide2");
+    $(".goToSlide2 .animsec3 .graphic-column1 .demo-sub-heading")[0].addEventListener( 'animationend', readytodemorealtime, false );  
+}
+
+var readytodemorealtime=function(e){
+    $(".animsec1 .mobile-screen-inn").addClass("goToMblSlide3"); 
+    $(".animsec1 .goToMblSlide3 .mbl-slide3 .mbl-app-notify-bubble")[0].addEventListener( 'webkitTransitionEnd', finishingrealtime, false );
+}
+
+var finishingrealtime=function(){
+    $(".trending-slides").addClass("finishrealtime"); 
+    $(".finishrealtime .animsec3 .graphic-column3 .demo-sub-heading2")[0].addEventListener( 'animationend', onrealtimeend, false );
+}
+
+var onrealtimeend=function (e) {
+    $(".trending-slides").addClass("goToSlide3");
+}
+
